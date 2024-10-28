aaf9cc1bafb7b4fbf658e894004e697c15dc177c
Torgbe-Desmond <123torgbe@gmail.com>
2024-10-28 09:08:05 -0700

    fixed bug in app.js

--- app.js
+++ app.js
@@ -5,24 +5,28 @@ require('dotenv').config();
 const connect = require('./db/db');
 require('express-async-errors');
 
-const PORT = process.env.PORT ||  4000 
+const PORT = process.env.PORT || 4000;
 
-app.use(cors);
-app.use(express.json());
+app.use(cors()); // Initialize CORS middleware
+app.use(express.json()); // Parse JSON requests
 
+// Routes
 app.use('/api/v1/contact', require('./routes/contact'));
 
-// error middlewares
-app.use(require('./middleware/errorMiddleware'));
-app.use(require('./middleware/notFound'))
+// Error handling middleware
+app.use(require('./middleware/notFound')); // Not found middleware
+app.use(require('./middleware/errorMiddleware')); // Error middleware
 
-const start= async()=>{
-    await connect(process.env.MONGO_URI)
-    app.listen(PORT,()=>{
-        console.log(`App is running on port ${PORT}`)
-    })
-}   
+// Start server and connect to DB
+const start = async () => {
+    try {
+        await connect(process.env.MONGO_URI); // Connect to MongoDB
+        app.listen(PORT, () => {
+            console.log(`App is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error);
+    }
+};
 
-start()
-
-// The uri is private so i will not show it here
+start();
